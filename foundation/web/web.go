3bf08cd0ddb014510139da23764c19ea18275aa1
laoye_jnr <idrisaboladeo@gmail.com>
2022-05-10 21:56:41 +0100

    :sparkles: custom Router
    wraps custom router around httptreemusk to handle routing, shutdown among others

--- foundation/web/web.go
+++ foundation/web/web.go
@@ -5,7 +5,7 @@ import (
 	// "context"
 	// "net/http"
 	"os"
-	// "syscall"
+	"syscall"
 	// "time"
 
 	"github.com/dimfeld/httptreemux/v5"
@@ -22,4 +22,19 @@ import (
 type App struct {
     *httptreemux.ContextMux
 	shutdown chan os.Signal
+}
+
+
+// NewApp creates an App value that handle a set of routes for the application.
+func NewApp(shutdown chan os.Signal) *App{
+	return &App{
+		ContextMux: httptreemux.NewContextMux(),
+		shutdown: shutdown,
+	}
+}
+
+// SignalShutdown is used to gracefully shut down the app when an integrity
+// issue is identified.
+func (a *App) SignalShutdown() {
+	a.shutdown <- syscall.SIGTERM
 }
