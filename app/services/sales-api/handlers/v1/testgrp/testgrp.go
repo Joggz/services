6c7913f6c43eab96a608bc907d7d5dc356c6149f
laoye_jnr <idrisaboladeo@gmail.com>
2022-05-12 17:16:16 +0100

    :sparkles: updates request context
    add traceID, time, statuscode into ctx

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -21,10 +21,7 @@ func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Reque
 	}{
 		Status: "OK",
 	}
-	statusCode := http.StatusOK
-
 	
-	h.Log.Infow(" Test  readiness", "statusCode", statusCode, "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
 	return  web.Respond(ctx, w, data, http.StatusOK)	
 	// json.NewEncoder(w).Encode(data)
 }
