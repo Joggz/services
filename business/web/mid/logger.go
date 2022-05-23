29526acd5e83a4234f28a71bf37a957069c69e8f
laoye_jnr <idrisaboladeo@gmail.com>
2022-05-23 17:37:27 +0100

    :memo: review

--- business/web/mid/logger.go
+++ business/web/mid/logger.go
@@ -18,6 +18,7 @@ func Logger(log *zap.SugaredLogger) web.Middleware {
 	m := func(handler web.Handler) web.Handler {
 		log.Infow("request handler", handler)
 
+		
 		// Create the handler that will be attached in the middleware chain.
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
